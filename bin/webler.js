de609d65c24aed5fc691c8f412df9929d7d7aa37
Rudá Robson <rudarobson@gmail.com>
2015-05-01 15:36:00 -0300

    adding razor and fluent api

--- bin/webler.js
+++ bin/webler.js
@@ -2,65 +2,110 @@ var components = require('../lib/components/components');
 var build = require('../lib/build/build');
 var handlebars = require('../lib/handlebars/handlebars');
 var markdown = require('../lib/markdown/markdown');
+var razor = require('node-razor');
+
 var tasker = require('../lib/build/tasker');
 var fs = require('fs');
 var utils = require('../lib/utils/utils.js');
 var path = require('path');
 
 
+
 var modules = {
-  build: function(content, srcDir, dst, options) {
-    var res = build.parse(content, srcDir, dst, options);
-    return res;
-  },
-  components: function(cnt, options) {
-    return components.parse(cnt, options);
-  },
-  handlebars: function(cnt, options) {
-    return handlebars.parse(cnt, options);
-  },
-  markdown:function(cnt, options) {
-    return markdown.parse(cnt, options);
-  },
-}
+  build: function(content, options, src, dst) {
+    var resp = build.parse(content, options, src, dst);
 
-var defaultExec = ['markdown', 'components', 'build', 'handlebars']
-module.exports = {
-  weble: function(src, dst, options, exec) {
-
-    if (!options)
-      options = {};
-    if (!exec)
-      exec = defaultExec;
-
-    var content = fs.readFileSync(src).toString();
-
-
-    for (var i in exec) {
-      var res = modules[exec[i]](content, options[exec[i]], src, dst);
-      switch (exec[i]) {
-        case 'build':
-          content = res.content;
-          var tasks = res.tasks;
-          for (var i in tasks) {
-            var task = tasks[i];
-            for (var j in task) {
-              var res = tasker.exec(i, task[j].sources, task[j].options);
-              utils.safeWriteFile(task[j].dest, res);
-            }
-          }
-          break;
-        case 'components':
-        case 'handlebars':
-        case 'markdown':
-          content = res;
-          break;
-        default:
-          console.log('Unsuported command!');
-          throw 'Unsuported command!';
+    var tasks = resp.tasks;
+    for (var i in tasks) {
+      var task = tasks[i];
+      for (var j in task) {
+        var res = tasker.exec(i, task[j].sources, task[j].options);
+        utils.safeWriteFile(task[j].dest, res);
       }
     }
 
+    return resp.content;
+  },
+  handlebars: function(content, options) {
+    return components.parse(content, options);
+  },
+  components: function(content, options) {
+    return components.parse(content, options);
+  },
+  razor: function(src, options) {
+    var opt = {};
+
+    if (options)
+      util.mergeObjects(opt, options);
+    opt.src = src;
+
+    return razor.parse(opt);
+  },
+  markdown: function(content, options) {
+    return markdown.parse(content, options);
+  }
+};
+
+function Webler(src, dst, options) {
+  if (!options)
+    options = {};
+
+  var content = fs.readFileSync(src).toString();
+
+  var parsers = {
+    build: function(opt) {
+      if (!opt) {
+        opt = options.build || {};
+      }
+      content = modules.build(content, opt, src, dst);
+      return parsers;
+    },
+    handlebars: function(opt) {
+      if (!opt) {
+        opt = options.handlebars || {};
+      }
+      content = modules.handlebars(content, opt, src, dst);
+      return parsers;
+    },
+    components: function(opt) {
+      if (!opt) {
+        opt = options.components || {};
+      }
+      content = modules.components(content, opt, src, dst);
+      return parsers;
+    },
+    razor: function(opt) {
+      if (!opt) {
+        opt = options.razor || {};
+      }
+      content = modules.razor(content, opt, src, dst);
+      return parsers;
+    },
+    markdown: function(opt) {
+      if (!opt) {
+        opt = options.markdown || {};
+      }
+      content = modules.markdown(content, opt, src, dst);
+      return parsers;
+    }
+  }
+
+  this.compile = function() {
+    return parsers;
+  }
+
+  this.render = function() {
     utils.safeWriteFile(dst, content);
+  }
+}
+
+
+module.exports = {
+  weble: function(src, dst, options) {
+    if (typeof(options) == typeof('')) { //this is the file configuration path
+      var f = require(options);
+      return f(new Webler(src, dst));
+    } else
+      return new Webler(src, dst, options);
   }
 }
--- test/nodeunit.js
+++ test/nodeunit.js
@@ -2,54 +2,62 @@ var fs = require('fs');
 var webler = require('../bin/webler');
 
 
-function test(assert, tests, exec) {
-	var src;
-	var res;
-	var dest;
-	var expected;
-	var actual;
+function test(assert, tests, moduleName) {
+  var src;
+  var res;
+  var dest;
+  var expected;
+  var actual;
 
-	for (var i in tests) {
-		var test = tests[i]
+  for (var i in tests) {
+    var test = tests[i]
 
-		webler.weble(test.src, test.dest, test.options, exec);
+    webler.weble(test.src, test.dest, test.options).compile()[moduleName]();
 
-		for (var j in test.assert) {
-			actual = fs.readFileSync(test.assert[j].actual).toString();
-			expected = fs.readFileSync(test.assert[j].expected).toString();
-			assert.equals(actual, expected, test.description);
-		}
-	}
+    for (var j in test.assert) {
+      actual = fs.readFileSync(test.assert[j].actual).toString();
+      expected = fs.readFileSync(test.assert[j].expected).toString();
+      assert.equals(actual, expected, test.description);
+    }
+  }
 }
 
-module.exports.testBuildBlock = function(assert) {
-	var tests = require('./nodeunit/build')();
+module.exports.build = function(assert) {
+  var tests = require('./nodeunit/build')();
 
-	assert.expect(18);
-	test(assert, tests, ['build']);
-	assert.done();
+  assert.expect(18);
+  test(assert, tests, 'build');
+  assert.done();
 };
 
 module.exports.components = function(assert) {
-	var tests = require('./nodeunit/components')();
+  var tests = require('./nodeunit/components')();
 
-	assert.expect(9);
-	test(assert, tests, ['components']);
-	assert.done();
+  assert.expect(9);
+  test(assert, tests, 'components');
+  assert.done();
 };
 
 module.exports.handlebars = function(assert) {
-	var tests = require('./nodeunit/handlebars')();
+  var tests = require('./nodeunit/handlebars')();
 
-	assert.expect(4);
-	test(assert, tests, ['handlebars']);
-	assert.done();
+  assert.expect(4);
+  test(assert, tests, 'handlebars');
+  assert.done();
 };
 
 module.exports.markdown = function(assert) {
-	var tests = require('./nodeunit/markdown')();
+  var tests = require('./nodeunit/markdown')();
 
-	assert.expect(2);
-	test(assert, tests, ['markdown']);
-	assert.done();
+  assert.expect(2);
+  test(assert, tests, 'markdown');
+  assert.done();
+};
+
+module.exports.razor = function(assert) {
+  var tests = require('./nodeunit/razor')();
+
+  assert.expect(2);
+  test(assert, tests, 'razor');
+  assert.done();
 };
