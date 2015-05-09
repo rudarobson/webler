389026a27830335c0ef5251607daf734aa681de9
Rudá Robson <rudarobson@gmail.com>
2015-05-08 22:00:23 -0300

    razor added support to virtual path few fixes to bundle and webler

--- tests/massive_tests/apps/app1/Webler.js
+++ tests/massive_tests/apps/app1/Webler.js
@@ -1,18 +1,30 @@
+function registerBundles(bundles) {
+  bundles.add('scripts', '~/Home/Index/index.js')
+    .include('js', '~/Pages/Home/Index/js/index.js');
+}
+
 module.exports = function(config) {
   var unit = this.weble({
     cwd: 'src/Pages',
     src: '**/*.cshtml',
     dest: 'tests_results'
+  }, {
+    virtualPath: {
+      src: 'src',
+      dest: 'tests_result'
+    }
   });
 
+  registerBundles(unit.bundles());
+
   unit.compile().razor({
-    layoutsPath: 'src/Layouts',
-    startupPath: 'src/Startup.cs'
-  })
-  .components({
-    componentsPath:'src/Components'
-  })
-  .markdown();
+      layoutsPath: 'src/Layouts'
+    })
+    .bundle()
+    .components({
+      componentsPath: 'src/Components'
+    })
+    .markdown();
 
   unit.render();
 }
