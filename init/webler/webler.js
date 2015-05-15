1437e3e5416686dd3daef01858135d218badafd4
Rudá Robson <rudarobson@gmail.com>
2015-05-15 17:42:57 -0300

    before changing to node-glob-expand

--- init/webler/webler.js
+++ init/webler/webler.js
@@ -1,3 +1,16 @@
-module.exports.develop = function(webler) {
 
+function registerBundles(bundles) {
+  /** Register yout bundles here **/
+  /**
+    bundles.add('scripts', '~/Destination')
+    .include('js', '~/script1.js')
+    .include('js', '~/script2.js');
+  **/
+}
+
+module.exports.develop = function(webler) {
+  webler.loadModule('bundle');
+  registerBundles(webler.api('bundle').bundles);
+
+  weble.weble()
 }
