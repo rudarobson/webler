80e2a83c3de278069800c8a6d4abc4314f5545b4
Rudá Robson <rudarobson@gmail.com>
2015-05-15 22:52:45 -0300

    changes to init

--- init/module/package/main.js
+++ init/module/package/main.js
@@ -1,16 +1,10 @@
 module.exports = {
-  type:'stream',
-  start: function(options) {//runtime arguments with options
+  type: 'stream',//type maybe stream, or bulk
+  start: function(options) { //runtime arguments with options
 
-  },
-  setup:function(){//this is called after plugin is loaded and only once
-
-  },
-  config: function(options) {//user calls it to override defaults options with options argument "user may call it many times"
-
-  },
-  cleanUp: function() {
-
-  },
-  api:undefined
+  }
+  //setup: function(){}, //this function is called after plugin is loaded and only once
+  //config: function(options){}, //this function is called by the user to override defaults options with the options argument "user may call it many times"
+  //cleanUp: function(){}, // this function is called by the user and must cleanup cached stuff between multiple webler.weble() calls, if any
+  //api: undefined //this might be anything you want to expose to the user
 }
