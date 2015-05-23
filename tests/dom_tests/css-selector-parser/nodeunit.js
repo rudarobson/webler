c37a93eca9a68e2667b2ec3c45e66e2db8ef0e9f
Rudá Robson <rudarobson@gmail.com>
2015-05-23 11:16:40 -0300

    more tests on css selector

--- tests/dom_tests/css-selector-parser/nodeunit.js
+++ tests/dom_tests/css-selector-parser/nodeunit.js
@@ -3,8 +3,126 @@ var domBasePath = path.join(path.dirname(require.resolve('webler')), 'dom');
 var css = require(path.join(domBasePath, 'css-selector-parser/parser'));
 
 module.exports.test0 = function(assert) {
+  var query = 'div';
+  var obj = css.parse(query)[0];
+  var match = obj.match;
+  var parents = obj.parents;
+
+  assert.equals(match.tagName, 'div');
+  assert.equals(parents.length, 0);
+  assert.done();
+}
+
+module.exports.test1 = function(assert) {
+  var query = '.class';
+  var obj = css.parse(query)[0];
+  var match = obj.match;
+  var parents = obj.parents;
+
+  assert.equals(match.classes[0], 'class');
+  assert.equals(parents.length, 0);
+  assert.done();
+}
+
+module.exports.test2 = function(assert) {
+  var query = 'div[attribute]';
+  var obj = css.parse(query)[0];
+  var match = obj.match;
+  var parents = obj.parents;
+  assert.equals(match.tagName, 'div');
+  assert.equals(match.attributes.length, 1);
+  assert.equals(match.attributes[0].type, css.attrTypes.has);
+
+  assert.done();
+}
+
+module.exports.test3 = function(assert) {
+  var query = 'div[attribute="value"]';
+  var obj = css.parse(query)[0];
+  var match = obj.match;
+  var parents = obj.parents;
+  assert.equals(match.tagName, 'div');
+  assert.equals(match.attributes.length, 1);
+  assert.equals(match.attributes[0].type, css.attrTypes.exactlyEqual);
+  assert.equals(match.attributes[0].name, 'attribute');
+  assert.equals(match.attributes[0].value, 'value');
+
+  assert.done();
+}
+
+module.exports.test4 = function(assert) {
+  var query = 'div[attribute~="value"]';
+  var obj = css.parse(query)[0];
+  var match = obj.match;
+  var parents = obj.parents;
+  assert.equals(match.tagName, 'div');
+  assert.equals(match.attributes.length, 1);
+  assert.equals(match.attributes[0].type, css.attrTypes.blankSepparated);
+  assert.equals(match.attributes[0].name, 'attribute');
+  assert.equals(match.attributes[0].value, 'value');
+
+  assert.done();
+}
+
+module.exports.test5 = function(assert) {
+  var query = 'div[attribute^="value"]';
+  var obj = css.parse(query)[0];
+  var match = obj.match;
+  var parents = obj.parents;
+  assert.equals(match.tagName, 'div');
+  assert.equals(match.attributes.length, 1);
+  assert.equals(match.attributes[0].type, css.attrTypes.beginsWith);
+  assert.equals(match.attributes[0].name, 'attribute');
+  assert.equals(match.attributes[0].value, 'value');
+
+  assert.done();
+}
+
+module.exports.test6 = function(assert) {
+  var query = 'div[attribute$="value"]';
+  var obj = css.parse(query)[0];
+  var match = obj.match;
+  var parents = obj.parents;
+  assert.equals(match.tagName, 'div');
+  assert.equals(match.attributes.length, 1);
+  assert.equals(match.attributes[0].type, css.attrTypes.endsWith);
+  assert.equals(match.attributes[0].name, 'attribute');
+  assert.equals(match.attributes[0].value, 'value');
+
+  assert.done();
+}
+
+module.exports.test7 = function(assert) {
+  var query = 'div[attribute*="value"]';
+  var obj = css.parse(query)[0];
+  var match = obj.match;
+  var parents = obj.parents;
+  assert.equals(match.tagName, 'div');
+  assert.equals(match.attributes.length, 1);
+  assert.equals(match.attributes[0].type, css.attrTypes.containsSubstring);
+  assert.equals(match.attributes[0].name, 'attribute');
+  assert.equals(match.attributes[0].value, 'value');
+
+  assert.done();
+}
+
+module.exports.test8 = function(assert) {
+  var query = 'div[attribute|="value"]';
+  var obj = css.parse(query)[0];
+  var match = obj.match;
+  var parents = obj.parents;
+  assert.equals(match.tagName, 'div');
+  assert.equals(match.attributes.length, 1);
+  assert.equals(match.attributes[0].type, css.attrTypes.hyphenSeppareted);
+  assert.equals(match.attributes[0].name, 'attribute');
+  assert.equals(match.attributes[0].value, 'value');
+
+  assert.done();
+}
+
+module.exports.test10 = function(assert) {
   var query = '.class1 > a-child';
-  var obj = css.parse(query);
+  var obj = css.parse(query)[0];
   var match = obj.match;
   var parents = obj.parents;
 
