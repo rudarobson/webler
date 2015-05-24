ff7f632f37f6280d3dbe17ae40818de730137c02
Rudá Robson <rudarobson@gmail.com>
2015-05-23 21:50:40 -0300

    must correct attribute selection

--- tests/dom_tests/css-engine/nodeunit.js
+++ tests/dom_tests/css-engine/nodeunit.js
@@ -10,12 +10,72 @@ var dom1 = htmlParser.parse(fs.readFileSync('./srcs/src1.html').toString());
 module.exports.test1 = function(assert) {
   var elts = dom1.filter('.class2 span');
 
-  assert.equals(elts.length,1);
-  assert.equals(elts[0].tagName,'span');
+  assert.equals(elts.length, 1);
+  assert.equals(elts[0].tagName, 'span');
+  assert.done();
+}
+module.exports.test2 = function(assert) {
+  var elts = dom1.filter('#spanwithid'); //changing sensitive case  of document
+  assert.equals(elts.length, 1);
+  assert.done();
+}
 
-  elts = dom1.filter('#spanwithid');//changing sensitive case  of document
-  assert.equals(elts.length,1);
-  //assert.equals(elts[0].tagName,'span');
-  //assert.equals(elts[0].attr('id'),'spanwithid');
+module.exports.test3 = function(assert) {
+  var elts = dom1.filter('[attribute1]');
+
+  assert.equals(elts.length, 2);
+  assert.equals(elts[0].tagName, 'div');
+  assert.equals(elts[1].tagName, 'span');
+  assert.done();
+}
+
+module.exports.test4 = function(assert) {
+  var elts = dom1.filter('[attribute1="value"]');
+
+  assert.equals(elts.length, 1);
+  assert.equals(elts[0].tagName, 'div');
+  assert.done();
+}
+
+module.exports.test5 = function(assert) {
+  var elts = dom1.filter('[class~="div-class1"]');
+
+  assert.equals(elts.length, 1);
+  assert.equals(elts[0].tagName, 'div');
+  assert.done();
+}
+
+module.exports.test6 = function(assert) {
+  var elts = dom1.filter('[attribute1^="value"]');
+
+  assert.equals(elts.length, 2);
+  assert.equals(elts[0].tagName, 'div');
+  assert.equals(elts[1].tagName, 'span');
+  assert.done();
+}
+
+module.exports.test7 = function(assert) {
+  var elts = dom1.filter('[attribute1$="alue1"]');
+
+  assert.equals(elts.length, 1);
+  assert.equals(elts[0].tagName, 'span');
+  assert.done();
+}
+
+module.exports.test8 = function(assert) {
+  var elts = dom1.filter('[attribute1*="alue"]');
+
+  assert.equals(elts.length, 2);
+  assert.equals(elts[0].tagName, 'div');
+  assert.equals(elts[1].tagName, 'span');
+  assert.done();
+}
+
+
+module.exports.test9 = function(assert) {
+  var elts = dom1.filter('[attribute2|="value1"]');
+
+  assert.equals(elts.length, 1);
+  assert.equals(elts[0].tagName, 'span');
   assert.done();
 }
