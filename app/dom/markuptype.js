6673387a0884aa6d955a9ab682d1041e22e10629
Rudá Robson <rudarobson@gmail.com>
2015-05-30 17:29:26 -0300

    deleting files

--- app/dom/markuptype.js
+++
@@ -1,6 +0,0 @@
-module.exports.element = 'element';
-module.exports.text = 'text';
-module.exports.doctype = 'doctype';
-module.exports.comment = 'comment';
-module.exports.cdata = 'cdata';
-module.exports.document = 'document';
